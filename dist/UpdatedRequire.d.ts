cba03e7bb8c1aeba06e5ffa70458babc58516029
Llorx <dallorx@gmail.com>
2016-11-20 11:25:26 +0100

    recursive fix

--- dist/UpdatedRequire.d.ts
+++ dist/UpdatedRequire.d.ts
@@ -1,10 +1,7 @@
 import { WatcherRequire, WatcherOptions, CustomNodeModule } from "watcher-require";
-export interface UpdatedOptions extends WatcherOptions {
-    recursive?: boolean;
-}
 export declare class UpdatedRequire extends WatcherRequire {
     _notifyCallback: (oldmodule: CustomNodeModule, newmodule: CustomNodeModule) => void;
     _updatedTimeouts: any;
-    constructor(notifyCallback?: (oldmodule: CustomNodeModule, newmodule: CustomNodeModule) => void, options?: UpdatedOptions);
+    constructor(notifyCallback?: (oldmodule: CustomNodeModule, newmodule: CustomNodeModule) => void, options?: WatcherOptions);
     requireNotify(mod: CustomNodeModule): void;
 }
